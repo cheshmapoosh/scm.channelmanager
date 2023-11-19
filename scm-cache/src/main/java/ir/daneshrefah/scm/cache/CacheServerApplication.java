481b9f9270e22e2548946edf45839b9560c980a1
Reza Jamshidi <jamshidi.r@dpi.ir>
2023-11-19 16:20:49 +0330

    add cache server

--- scm-cache/src/main/java/ir/daneshrefah/scm/cache/CacheServerApplication.java
+++ scm-cache/src/main/java/ir/daneshrefah/scm/cache/CacheServerApplication.java
@@ -3,7 +3,6 @@ package ir.daneshrefah.scm.cache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * Description of the class or purpose of the file.
---
+++ scm-cache/src/main/java/ir/daneshrefah/scm/cache/config/HazelcastConfig.java
@@ -0,0 +1,14 @@
+package ir.daneshrefah.scm.cache.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Description of the class or purpose of the file.
+ *
+ * @author reza jamshidi
+ * @version 1.0
+ * @since 2023-11-19
+ */
+@Configuration
+public class HazelcastConfig {
+}
