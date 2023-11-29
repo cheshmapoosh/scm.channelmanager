a5067c9238d5f07edf1374d7cc3a38ef2a389a7f
Reza Jamshidi <jamshidi.r@dpi.ir>
2023-11-29 14:09:46 +0330

    fix scm-cache

--- scm-cache/src/main/java/ir/daneshrefah/scm/cache/service/HazelCastService.java
+++ scm-cache/src/main/java/ir/daneshrefah/scm/cache/service/HazelCastService.java
@@ -1,12 +1,26 @@
 package ir.daneshrefah.scm.cache.service;
 
-import ir.daneshrefah.scm.cache.client.CacheTemplate;
+import com.hazelcast.flakeidgen.FlakeIdGenerator;
 
 import java.util.List;
+import java.util.Map;
 
-public interface HazelCastService extends CacheTemplate {
+public interface HazelCastService {
 
     List<String> getMapList();
 
     Object getMapData(String map);
+
+    Object getFromCache(String mapName, String key);
+
+    void putInCache(String mapName, String key, Object value);
+
+    void putInCache(String mapName, String key, Object value, int timeToLiveSeconds);
+
+    Object removeFromCache(String mapName, String key);
+
+    Map createCacheIfNull(String mapName);
+
+    FlakeIdGenerator createIdGeneratorIfNull(String name);
+
 }
