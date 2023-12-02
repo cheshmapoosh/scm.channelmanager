7e11c127eb208b5692d90a8ca597f28887fb626e
Reza Jamshidi <jamshidi.r@dpi.ir>
2023-12-02 12:18:10 +0330

    fix scm-config

---
+++ scm-config/src/main/java/ir/daneshrefah/scm/config/EnvironmentRepositoryConfiguration.java
@@ -0,0 +1,25 @@
+package ir.daneshrefah.scm.config;
+
+import org.springframework.cloud.config.server.environment.JdbcEnvironmentProperties;
+import org.springframework.cloud.config.server.environment.JdbcEnvironmentRepository;
+import org.springframework.cloud.config.server.environment.JdbcEnvironmentRepositoryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Description of the class or purpose of the file.
+ *
+ * @author reza jamshidi
+ * @version 1.0
+ * @since 2023-12-02
+ */
+@Configuration
+public class EnvironmentRepositoryConfiguration {
+
+    @Bean
+    public JdbcEnvironmentRepository jdbcEnvironmentRepository(JdbcEnvironmentRepositoryFactory factory,
+                                                               JdbcEnvironmentProperties environmentProperties) {
+        return factory.build(environmentProperties);
+    }
+
+}
