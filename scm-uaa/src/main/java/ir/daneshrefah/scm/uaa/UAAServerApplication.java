d7963bd0d1d7d66c1d009bd42781088d65643d0f
Reza Jamshidi <jamshidi.r@dpi.ir>
2023-12-13 18:15:21 +0330

    add scm-uaa

---
+++ scm-uaa/src/main/java/ir/daneshrefah/scm/uaa/UAAServerApplication.java
@@ -0,0 +1,22 @@
+package ir.daneshrefah.scm.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * Description of the class or purpose of the file.
+ *
+ * @author reza jamshidi
+ * @version 1.0
+ * @since 2023-11-18
+ */
+@SpringBootApplication
+@EnableCaching
+public class UAAServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UAAServerApplication.class, args);
+    }
+}
+
