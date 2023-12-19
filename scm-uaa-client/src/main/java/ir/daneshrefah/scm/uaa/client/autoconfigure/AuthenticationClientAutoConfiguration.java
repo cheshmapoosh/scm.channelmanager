0d1f9095f2671f62b626613dc57610c0d4ee0bbf
Reza Jamshidi <jamshidi.r@daneshrefah.ir>
2023-12-19 14:17:06 +0330

    uaa-client

---
+++ scm-uaa-client/src/main/java/ir/daneshrefah/scm/uaa/client/autoconfigure/AuthenticationClientAutoConfiguration.java
@@ -0,0 +1,22 @@
+package ir.daneshrefah.scm.uaa.client.autoconfigure;
+
+import ir.daneshrefah.scm.uaa.client.core.AuthenticationClientTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Description of the class or purpose of the file.
+ *
+ * @author reza jamshidi
+ * @version 1.0
+ * @since 2023-12-19
+ */
+@Configuration
+public class AuthenticationClientAutoConfiguration {
+
+
+    @Bean
+    public AuthenticationClientTemplate authenticationClientTemplate() {
+        return new AuthenticationClientTemplate();
+    }
+}
---
+++ scm-uaa-client/src/main/java/ir/daneshrefah/scm/uaa/client/properties/AuthenticationClientAutoProperties.java
@@ -0,0 +1,11 @@
+package ir.daneshrefah.scm.uaa.client.properties;
+
+/**
+ * Description of the class or purpose of the file.
+ *
+ * @author reza jamshidi
+ * @version 1.0
+ * @since 2023-12-19
+ */
+public class AuthenticationClientAutoProperties {
+}
