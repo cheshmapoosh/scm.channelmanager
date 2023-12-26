95712251e8ebeebc32878eb743b8102a6c6a8399
Dariush Abdolahi <abdollahi.d@dpi.ir>
2023-12-26 07:54:07 +0000

    Scm abdollahi acl

--- scm-core/src/main/java/ir/daneshrefah/scm/core/authority/decision/voter/DecisionVoter.java
+++ scm-core/src/main/java/ir/daneshrefah/scm/core/authority/decision/voter/DecisionVoter.java
@@ -12,12 +12,22 @@ import java.util.List;
 
 public interface DecisionVoter {
     List<DecisionVoter>  DECISION_VOTER_LIST = new ArrayList<>();
+    /**
+     * if any voter return a ACCESS_GRANTED value, the manager accept it and end the checking another voter
+     */
     int ACCESS_GRANTED = 1;
+    /**if the voter return ACCESS_ABSTAIN it means the voter accepted but manager must check another voters */
     int ACCESS_ABSTAIN = 0;
+    /**
+     * if the voter return ACCESS_DENIED or throws any AuthorityBaseException it means that rule does not passed and
+     * the manager stopped the checking another voters.
+     */
     int ACCESS_DENIED = -1;
 
     int vote(Message message, TerminalServiceChannelAccess authObject) throws AuthorityBaseException;
-
+    /**
+     * the manager order the voters by this priority.
+     */
     Priority priority();
 
     @PostConstruct
