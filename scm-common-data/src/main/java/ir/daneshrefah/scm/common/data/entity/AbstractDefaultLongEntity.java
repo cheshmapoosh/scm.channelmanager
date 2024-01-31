42a0171ffd7ed4d1d823d5f866246b2a0d6aafd8
reza <reza@daneshrefah.ir>
2024-01-31 09:46:46 +0330

    scm-core => add AbstractDefaultLongEntity

--- scm-common-data/src/main/java/ir/daneshrefah/scm/common/data/entity/AbstractDefaultLongEntity.java
+++ scm-common-data/src/main/java/ir/daneshrefah/scm/common/data/entity/AbstractDefaultLongEntity.java
@@ -1,11 +1,8 @@
 package ir.daneshrefah.scm.common.data.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.time.LocalDateTime;
 
 /**
  * Description of the class or purpose of the file.
@@ -17,15 +14,6 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @MappedSuperclass
-public abstract class AbstractDefaultLongEntity extends AbstractEntity<Long> {
-
-    @Column(name = "CREATOR", updatable = false)
-    private String creator;
-    @Column(name = "LAST_EDITOR")
-    private String lastEditor;
-    @Column(name = "CREATE_DATE", insertable = false, updatable = false)
-    private LocalDateTime createDate;
-    @Column(name = "LAST_EDIT_DATE", insertable = false)
-    private LocalDateTime lastEditDate;
+public abstract class AbstractDefaultLongEntity extends AbstractDefaultEntity<Long> {
 
 }
