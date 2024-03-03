6804f570973d09bd5c525c63bef7677059daf210
Behzad Bakhshayesh <b.sedigh@dpi.ir>
2024-03-03 04:09:44 +0000

    feature/NIB-277-tab-converters

--- scm-uaa/src/main/resources/static/assets/js/converter.js
+++ scm-uaa/src/main/resources/static/assets/js/converter.js
@@ -2,16 +2,28 @@ const tablinks = document.getElementsByClassName("tab-links");
 const tabcontents = document.getElementsByClassName("tab-contents");
 
 const opentab = (tabname) => {
+  console.log({ tabname });
   [...tablinks].forEach((tablink) => {
     tablink.classList.remove("active-link");
+    if (tablink.getAttribute("data-id") === tabname) {
+      tablink.classList.add("active-link");
+    }
   });
   [...tabcontents].forEach((tabcontent) => {
     tabcontent.classList.remove("active-tab");
+    if (tabcontent.getAttribute("id") === tabname) {
+      tabcontent.classList.add("active-tab");
+    }
   });
-  event.currentTarget.classList.add("active-link");
-  document.getElementById(tabname).classList.add("active-tab");
+  localStorage.setItem("active-tab", tabname);
 };
-
 [...tablinks].forEach((tablink) => {
   tablink.addEventListener("click", (e) => opentab(e.target.dataset.id));
 });
+const initTabname = localStorage.getItem("active-tab") ?? "tab1";
+opentab(initTabname);
+
+function cancelConsent() {
+  document.consent_form.reset();
+  document.consent_form.submit();
+}
