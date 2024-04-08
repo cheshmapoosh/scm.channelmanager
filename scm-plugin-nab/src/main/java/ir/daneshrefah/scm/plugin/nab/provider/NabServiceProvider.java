4f525e84e0faf09f909f5db8f6c4ab49f18c50f3
reza <reza@daneshrefah.ir>
2024-04-08 09:58:23 +0330

    scm-core => switch NabServiceProvider to parametric url

--- scm-plugin-nab/src/main/java/ir/daneshrefah/scm/plugin/nab/provider/NabServiceProvider.java
+++ scm-plugin-nab/src/main/java/ir/daneshrefah/scm/plugin/nab/provider/NabServiceProvider.java
@@ -55,15 +55,10 @@ public final class NabServiceProvider extends AbstractRestExternalServiceProvide
     @SneakyThrows
     protected String prepareTargetUrl(Message message) {
         ExternalService service = (ExternalService) message.getHeader().getServiceAccess().getService();
-        ExternalServiceProviderMetadata providerMetadata = getProvider().getMetadata();
+        String providerEndpoint = extractProviderEndpoint();
         JsonNode componentMetadata = service.getMetadata();
-        String target = StringUtils.appendIfMissing(providerMetadata.getEndpoint(), "/") + componentMetadata.get("serviceName").asText();
+        String target = providerEndpoint + StringUtils.removeStart(componentMetadata.get("serviceName").asText(), "/");
         return target;
-//        return "https://dummy.restapiexample.com/api/v1/employees";
-//            endpointUri = "http://${host}/Service/";
-//            Map<String, String> values = new HashMap<>();
-//            values.put("host", "scm-core.daneshrefah.ir");
-//            StringSubstitutor.replace(endpointUri, values);
     }
 
     @Override
