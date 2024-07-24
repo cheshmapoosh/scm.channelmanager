1cc9ec7965280711c0c9be10f871a3091d8c2ceb
reza <reza@daneshrefah.ir>
2024-07-24 12:56:49 +0330

    scm-uaa => change AuthenticationMethodModificationRequest
    Signed-off-by: reza <reza@daneshrefah.ir>

--- scm-uaa/src/main/java/ir/daneshrefah/scm/uaa/controller/user/AuthenticationMethodModificationRequest.java
+++ scm-uaa/src/main/java/ir/daneshrefah/scm/uaa/controller/user/AuthenticationMethodModificationRequest.java
@@ -4,11 +4,21 @@ import ir.daneshrefah.scm.common.dto.RequestData;
 import ir.daneshrefah.scm.common.model.user.AuthenticationMethod;
 import lombok.Data;
 
+/*
+ * TODO applyTransactionAuthentication
+ * */
 @Data
 public class AuthenticationMethodModificationRequest implements RequestData {
+
        private String username;
        private String terminalCode;
        private String otpCode;
        private String recipient;
        private AuthenticationMethod authenticationMethod;
+
+//       private AuthenticationMethod authenticationMethod;
+//       private String credential;
+//       /*
+//        * TODO send OTP service must be handle selected authentication method
+//        * */
 }
