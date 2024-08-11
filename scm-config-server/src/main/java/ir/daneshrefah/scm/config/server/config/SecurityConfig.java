57da7dbee944c0810a0409d3d4b2bc086890b098
Abbas Cheshmapoush <cheshmapoosh@gmail.com>
2024-08-11 13:59:58 +0330

    config server

--- scm-config-server/src/main/java/ir/daneshrefah/scm/config/server/config/SecurityConfig.java
+++ scm-config-server/src/main/java/ir/daneshrefah/scm/config/server/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package ir.daneshrefah.scm.config.server.config;
 
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.config.server.config.ConfigServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -12,6 +13,9 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.nio.file.Paths;
+import java.util.Objects;
 
 @Configuration
 @EnableWebSecurity
@@ -31,12 +35,20 @@ public class SecurityConfig {
     @Value("${scm.config.opr-user.name}")
     private String oprUserPass;
 
+    private final ConfigServerProperties configServerProperties;
+
+    public SecurityConfig(ConfigServerProperties configServerProperties) {
+        this.configServerProperties = configServerProperties;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        final String configPathRegx = Paths.get(Objects.toString(configServerProperties.getPrefix(), "/"), "**").toString();
         http
                 .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
                     authorizationManagerRequestMatcherRegistry
                             .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                            .requestMatchers(configPathRegx).hasAnyRole("ADMIN", "CONFIG")
                             .anyRequest().fullyAuthenticated();
                 })
                 .httpBasic(httpBasicCustomizer ->
@@ -46,7 +58,7 @@ public class SecurityConfig {
                                     response.addHeader("WWW-Authenticate", "Basic realm=\"Config Server\"");
                                     response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
                                 })
-                ).csrf(AbstractHttpConfigurer::disable) ;
+                ).csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
