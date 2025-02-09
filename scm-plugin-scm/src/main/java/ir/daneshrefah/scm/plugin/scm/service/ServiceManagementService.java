03a05d94896ee0adcefdc9b4cd969845b7bc0065
Dariush Abdulahi <abdolahi.d@daneshrefah.ir>
2025-02-09 10:29:35 +0000

    SCM-504:بروزرسانی و رفع باگ SCM

--- scm-plugin-scm/src/main/java/ir/daneshrefah/scm/plugin/scm/service/ServiceManagementService.java
+++ scm-plugin-scm/src/main/java/ir/daneshrefah/scm/plugin/scm/service/ServiceManagementService.java
@@ -119,35 +119,17 @@ public class ServiceManagementService extends AbstractJavaService {
 
     // COMPOSITE SERVICE
 
-    @JavaService(
-            serviceCode = SVC_SERVICE_CREATE_COMPOSITION,
-            path = "/composition/create",
-            parentCode = SVC_SERVICE_PARENT,
-            checkAccessFirstAuthentication = Status.ACTIVE,
-            type = JavaMethodType.ENTITY_CREATE
-    )
+    @JavaService(serviceCode = SVC_SERVICE_CREATE_COMPOSITION)
     public CompositionService createCompositionService(CompositionServiceCreateRequest request) {
         return (CompositionService) this.service.createCompositionService(request);
     }
 
-    @JavaService(
-            serviceCode = SVC_SERVICE_EDIT_COMPOSITION,
-            path = "/composition/edit",
-            parentCode = SVC_SERVICE_PARENT,
-            checkAccessFirstAuthentication = Status.ACTIVE,
-            type = JavaMethodType.ENTITY_UPDATE
-    )
+    @JavaService(serviceCode = SVC_SERVICE_EDIT_COMPOSITION)
     public CompositionService editCompositionService(CompositionServiceEditRequest request) {
         return (CompositionService) this.service.editCompositionService(request);
     }
 
-    @JavaService(
-            serviceCode = SVC_SERVICE_FIND_COMPOSITION,
-            path = "/composition/get",
-            parentCode = SVC_SERVICE_PARENT,
-            checkAccessFirstAuthentication = Status.ACTIVE,
-            type = JavaMethodType.INQUIRY
-    )
+    @JavaService(serviceCode = SVC_SERVICE_FIND_COMPOSITION)
     public CompositionService getCompositionService(String compositionServiceId) {
         return (CompositionService) this.service.getCompositionService(compositionServiceId);
     }
