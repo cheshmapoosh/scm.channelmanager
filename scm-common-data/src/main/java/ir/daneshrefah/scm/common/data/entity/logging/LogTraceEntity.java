ccdb07a0d032ef3c33f684d8489cfcb711a4c37f
A.Rayani <alireza.rayani@gmail.com>
2025-03-11 10:13:05 +0330

    refactoring in LogTraceEntity

--- scm-common-data/src/main/java/ir/daneshrefah/scm/common/data/entity/logging/LogTraceEntity.java
+++ scm-common-data/src/main/java/ir/daneshrefah/scm/common/data/entity/logging/LogTraceEntity.java
@@ -80,4 +80,6 @@ public class LogTraceEntity {
     private String payload;
     @Column(name = "ARCHIVE_NO", updatable = false)
     private Long archiveNo;
+    @Column(name = "ROW_NO")
+    private Integer rowNo;
 }
