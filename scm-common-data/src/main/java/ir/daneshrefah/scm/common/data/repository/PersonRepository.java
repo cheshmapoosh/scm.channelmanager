52556bc8c2607e81ecb7bac61ad06d2dd701a1ed
Abbas Cheshmapoush <cheshmapoosh@gmail.com>
2025-04-07 09:07:16 +0330

    SCMNEW-4

--- scm-common-data/src/main/java/ir/daneshrefah/scm/common/data/repository/PersonRepository.java
+++ scm-common-data/src/main/java/ir/daneshrefah/scm/common/data/repository/PersonRepository.java
@@ -39,7 +39,7 @@ public interface PersonRepository extends JpaRepository<GeneralPersonEntity, Lon
     List<GeneralPersonEntity> findPersonByUsername(String username);
 
     @Query("SELECT p FROM GeneralLegalPersonEntity p WHERE p.nationalId = :nationalId")
-    GeneralLegalPersonEntity findGeneralLegalPersonEntityByNationalId(@Param("nationalId") String nationalId);
+    Optional<List<GeneralLegalPersonEntity>> findGeneralLegalPersonEntityByNationalId(@Param("nationalId") String nationalId);
 
     @Query("SELECT p FROM GeneralLegalPersonEntity p WHERE p.nationalId = :nationalId AND p.personType = :personType")
     GeneralLegalPersonEntity findGeneralLegalPersonEntityByNationalIdAndPersonType(
