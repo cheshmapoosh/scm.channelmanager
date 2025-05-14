df2c121be6dd22d09497e6aea90014802fb2ae44
Dariush Abdolahi <dariush.abdolahi@gmail.com>
2025-05-14 14:24:41 +0430

    -add person otp by national id API. -add default profile to development environment

--- scm-common/src/main/java/ir/daneshrefah/scm/common/constant/otp/OtpReason.java
+++ scm-common/src/main/java/ir/daneshrefah/scm/common/constant/otp/OtpReason.java
@@ -40,7 +40,8 @@ public enum OtpReason {
     PAYMENT_TRANSFER_INTERNAL_BATCH                 (1,5,OtpPattern.NUMERIC,NotificationTemplate.GENERAL,2,1,21),
     PAYMENT_TRANSFER_INSURANCE_BATCH                (1,5,OtpPattern.NUMERIC,NotificationTemplate.GENERAL,2,1,22),
     PAYMENT_TRANSFER_RECURRING_ADD_ACH              (1,5,OtpPattern.NUMERIC,NotificationTemplate.GENERAL,2,1,23),
-    PAYMENT_TRANSFER_RECURRING_ADD_BATCH_ACH        (1,5,OtpPattern.NUMERIC,NotificationTemplate.GENERAL,2,1,24);
+    PAYMENT_TRANSFER_RECURRING_ADD_BATCH_ACH        (1,5,OtpPattern.NUMERIC,NotificationTemplate.GENERAL,2,1,24),
+    BANK_CONSOLE_CUSTOMER_VERIFICATION              (2,5,OtpPattern.NUMERIC,NotificationTemplate.GENERAL,2,1,25);
 
     private final int timeToLiveMinutes;
     private final int length;
