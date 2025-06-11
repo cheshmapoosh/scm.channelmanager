1191d1f003e76977652fe017f6b75b546f4e49a5
A.Rayani <alireza.rayani@gmail.com>
2025-06-11 16:08:49 +0330

    Bug fix and refactor in logging

---
+++ scm-logging/src/main/java/ir/daneshrefah/scm/log/model/LogTraceRequest.java
@@ -0,0 +1,35 @@
+package ir.daneshrefah.scm.log.model;
+
+import ir.daneshrefah.scm.common.dto.spec.PagedRequestData;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class LogTraceRequest extends PagedRequestData {
+    private String channelCode;
+    private String terminalCode;
+    private String clientId;
+    private String correlationId;
+    private String clientCorrelationId;
+    private String clientFlowId;
+    private String flowId;
+    private String messageId;
+    private String parentMessageId;
+    private String serviceCode;
+    private String nickname;
+    private String username;
+    private String delegatorUsername;
+    private String delegatorNickname;
+    private String hostAddress;
+    private Integer statusCode;
+    private Date startTime;
+    private Date endTime;
+    private String exceptionClassName;
+    private String endPoint;
+    private String amount;
+    private String accountNo;
+    private String cardNo;
+}
