9403ed4e6d4755b87b3dc51e71f51d9cd95b7e77
Dariush Abdolahi <dariush.abdolahi@gmail.com>
2025-07-26 09:44:00 +0430

    -add error sorted handlers cache.

--- scm-common-error/src/main/java/ir/daneshrefah/scm/common/error/management/ErrorHandlerChain.java
+++ scm-common-error/src/main/java/ir/daneshrefah/scm/common/error/management/ErrorHandlerChain.java
@@ -4,21 +4,28 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
+
 @RequiredArgsConstructor
 public abstract class ErrorHandlerChain {
+    private static final List<ErrorHandler> sortedErrorHandlers = new ArrayList<>();
     private final List<ErrorHandler> errorHandlers;
 
     public abstract List<Class<? extends ErrorHandler>> errorHandlerChain();
 
     public List<ErrorHandler> getOrdersErrorHandlers() {
-        List<ErrorHandler> orders = new ArrayList<>();
-        errorHandlerChain().forEach(aClass -> {
-            ErrorHandler foundHandler = errorHandlers
-                    .stream()
-                    .filter(errorHandler -> errorHandler.getClass().isAssignableFrom(aClass))
-                    .findFirst().orElseThrow(IllegalArgumentException::new);
-            orders.add(foundHandler);
-        });
-        return orders;
+        if (sortedErrorHandlers.isEmpty()) {
+            synchronized (sortedErrorHandlers) {
+                if (sortedErrorHandlers.isEmpty()) {
+                    errorHandlerChain().forEach(aClass -> {
+                        ErrorHandler foundHandler = errorHandlers
+                                .stream()
+                                .filter(errorHandler -> errorHandler.getClass().isAssignableFrom(aClass))
+                                .findFirst().orElseThrow(IllegalArgumentException::new);
+                        sortedErrorHandlers.add(foundHandler);
+                    });
+                }
+            }
+        }
+        return sortedErrorHandlers;
     }
 }
