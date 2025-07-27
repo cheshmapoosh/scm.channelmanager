56a365c20c39a71e73cb7c380f0bfb283dd596d0
Dariush Abdolahi <dariush.abdolahi@gmail.com>
2025-07-27 15:21:46 +0430

    -fix merge conflict

--- scm-core/src/main/java/ir/daneshrefah/scm/core/entity/condition/TerminalConditionEntity.java
+++ scm-core/src/main/java/ir/daneshrefah/scm/core/entity/condition/TerminalConditionEntity.java
@@ -1,6 +1,5 @@
 package ir.daneshrefah.scm.core.entity.condition;
 
-import ir.daneshrefah.scm.common.data.entity.gateway.ChannelEntity;
 import ir.daneshrefah.scm.common.data.entity.terminal.TerminalEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
