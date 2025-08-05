92c851119acf1613f939b3d4163c506d00e7756b
A.Rayani <alireza.rayani@gmail.com>
2025-08-05 12:06:07 +0330

    SCMNEW-44: Implement java service for transactionLog table

--- scm-logging/src/main/java/ir/daneshrefah/scm/log/service/LogTraceServiceImpl.java
+++ scm-logging/src/main/java/ir/daneshrefah/scm/log/service/LogTraceServiceImpl.java
@@ -1,22 +1,62 @@
-package ir.daneshrefah.scm.log.service;
+package ir.daneshrefah.scm.common.log.service;
 
-import ir.daneshrefah.scm.common.data.repository.logging.LogTraceRepository;
+import ir.daneshrefah.scm.common.dto.spec.PagedResponseData;
+import ir.daneshrefah.scm.common.exception.MissingRequiredInputException;
+import ir.daneshrefah.scm.common.log.entity.logging.LogTraceEntity;
+import ir.daneshrefah.scm.common.log.mapper.LogTraceMapper;
+import ir.daneshrefah.scm.common.log.model.LogTraceFindByIdRequest;
+import ir.daneshrefah.scm.common.log.model.LogTracePayloadResponse;
+import ir.daneshrefah.scm.common.log.model.LogTraceRequest;
+import ir.daneshrefah.scm.common.log.model.LogTraceResponse;
+import ir.daneshrefah.scm.common.log.repository.logging.LogTraceRepository;
+import ir.daneshrefah.scm.common.log.repository.logging.TraceLogSpec;
+import ir.daneshrefah.scm.common.log.utils.PageableUtils;
+import ir.daneshrefah.scm.utils.validation.ValidationUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class LogTraceServiceImpl implements LogService {
-    private final LogTraceRepository logTraceRepository;
-    private final ConverterService converterService;
 
-    public void save(String msg) {
+    private final LogTraceRepository logTraceRepository;
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void saveAll(List<LogTraceEntity> logTraces) {
+        logTraceRepository.saveAll(logTraces);
+    }
+
+    @Override
+    public PagedResponseData<LogTraceResponse> findAll(LogTraceRequest request) {
+        request = Objects.nonNull(request) ? request : new LogTraceRequest();
+        Pageable pageable = PageableUtils.getPageable(request);
+        Page<LogTraceEntity> entities = logTraceRepository.findAll(TraceLogSpec.toSpecification(request), pageable);
+        LogTraceMapper instance = LogTraceMapper.INSTANCE;
+        List<LogTraceResponse> logTraceResponseList = instance.toModelList(entities.getContent());
+        return new PagedResponseData<>(request.getPageNo(), request.getPageSize(), entities.getTotalElements(), logTraceResponseList);
+    }
+
+    @Override
+    public LogTracePayloadResponse getPayload(LogTraceFindByIdRequest request) {
         try {
-            logTraceRepository.saveAll(converterService.mapToLogTraceEntity(msg));
+            ValidationUtils.checkBlankString(request.getSpanId(), () -> new MissingRequiredInputException("spainId"));
+            ValidationUtils.checkBlankString(request.getTraceId(), () -> new MissingRequiredInputException("traceId"));
+            String payload = logTraceRepository.findAggregatedPayload(request.getSpanId(), request.getTraceId());
+            if (payload == null) {
+                return new LogTracePayloadResponse();
+            }
+            return new LogTracePayloadResponse(payload);
         } catch (Exception e) {
-            log.error("Failed to save message: {} due to error: {}", msg, e.getMessage(), e);
+            throw new RuntimeException("Failed to read CLOB result", e);
         }
     }
 }
