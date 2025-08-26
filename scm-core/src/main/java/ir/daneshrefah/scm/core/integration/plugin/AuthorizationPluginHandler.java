cec44bedfea1aa6a45c5909cb9daa6589d2ad7bf
Dariush Abdolahi <dariush.abdolahi@gmail.com>
2025-08-26 14:36:21 +0430

    - add authorization plugin

--- scm-core/src/main/java/ir/daneshrefah/scm/core/integration/plugin/AuthorizationPluginHandler.java
+++ scm-core/src/main/java/ir/daneshrefah/scm/core/integration/plugin/AuthorizationPluginHandler.java
@@ -1,37 +1,34 @@
-package ir.daneshrefah.scm.core.integration.plugin.general;
+package ir.daneshrefah.scm.core.integration.plugin;
 
 import ir.daneshrefah.scm.common.handler.PluginHandler;
 import ir.daneshrefah.scm.common.model.plugin.PluginDetail;
 import ir.daneshrefah.scm.common.model.plugin.PluginType;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.Exchange;
 import org.apache.camel.model.RouteDefinition;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
-import static ir.daneshrefah.scm.common.model.plugin.PluginType.TRANSFORMER;
-
 @Component
 @RequiredArgsConstructor
-@Slf4j
-public class OperationGlobalErrorHandlerPlugin implements PluginHandler {
+public class AuthorizationPluginHandler implements PluginHandler {
+    private static final String JWT_INSTANCE = "jwt";
+
     @Override
     public PluginType getType() {
-        return TRANSFORMER;
+        return PluginType.VALIDATOR;
     }
 
     @Override
     public void init(RouteDefinition routeDefinition, PluginDetail pluginDetail, Map<String, ?> properties) {
-        routeDefinition
-                .process(exchange -> {
-                    log.info("Processing Exchange Body");
-                });
+        System.out.println("salm");
+        properties.get("token");
     }
 
     @Override
     public void handle(Exchange exchange, PluginDetail pluginDetail) throws Exception {
-
+        Jwt jwt = (Jwt) exchange.getIn().getHeader(JWT_INSTANCE);
     }
 }
