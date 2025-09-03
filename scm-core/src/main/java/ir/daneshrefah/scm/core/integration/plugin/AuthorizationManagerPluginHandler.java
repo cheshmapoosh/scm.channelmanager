e0978096191d85cd3357fbcffd28ad126bf53e32
Dariush Abdolahi <dariush.abdolahi@gmail.com>
2025-09-03 12:22:54 +0430

    - add security plugin

--- scm-core/src/main/java/ir/daneshrefah/scm/core/integration/plugin/AuthorizationManagerPluginHandler.java
+++ scm-core/src/main/java/ir/daneshrefah/scm/core/integration/plugin/AuthorizationManagerPluginHandler.java
@@ -2,33 +2,39 @@ package ir.daneshrefah.scm.core.integration.plugin;
 
 import ir.daneshrefah.scm.common.handler.PluginHandler;
 import ir.daneshrefah.scm.common.model.plugin.PluginDetail;
+import ir.daneshrefah.scm.common.model.plugin.PluginPhase;
 import ir.daneshrefah.scm.common.model.plugin.PluginType;
+import ir.daneshrefah.scm.core.authority.decision.manager.AuthorizationDecisionManager;
 import lombok.RequiredArgsConstructor;
 import org.apache.camel.Exchange;
 import org.apache.camel.model.RouteDefinition;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
-public class AuthorizationPluginHandler implements PluginHandler {
-    private static final String JWT_INSTANCE = "jwt";
+public class AuthorizationManagerPluginHandler implements PluginHandler {
+
+    private final AuthorizationDecisionManager decisionManager;
 
     @Override
     public PluginType getType() {
-        return PluginType.TRANSFORMER;
+        return PluginType.VALIDATOR;
     }
 
     @Override
     public void init(RouteDefinition routeDefinition, PluginDetail pluginDetail, Map<String, ?> properties) {
-        System.out.println("salm");
-        properties.get("token");
+        //THIS PLUGIN MUST REGISTERED ON USAGE 'CHANNEL'
     }
 
+    /**
+     * @apiNote This method is used to decide the request is authentication and authorization or not.
+     */
     @Override
     public void handle(Exchange exchange, PluginDetail pluginDetail) throws Exception {
-        Jwt jwt = (Jwt) exchange.getIn().getHeader(JWT_INSTANCE);
+        if (pluginDetail.getPhase().equals(PluginPhase.BEFORE)){
+            decisionManager.decide(exchange);
+        }
     }
 }
