3889629ee45a8f6d909ca378f7a5b64a2eed2398
A.Rayani <alireza.rayani@gmail.com>
2025-10-21 14:13:15 +0330

    SCMNEW-44: Update LogConverterConfiguration to refine disabled logger functionality by adding `supports` method and ensuring proper handling of unsupported log messages.

--- scm-logging/src/main/java/ir/daneshrefah/scm/log/config/LogConverterConfiguration.java
+++ scm-logging/src/main/java/ir/daneshrefah/scm/log/config/LogConverterConfiguration.java
@@ -1,6 +1,7 @@
 package ir.daneshrefah.scm.log.config;
 
 import ch.qos.logback.classic.pattern.LoggerConverter;
+import ir.daneshrefah.scm.log.model.LogMessage;
 import ir.daneshrefah.scm.log.service.ConverterService;
 import jakarta.transaction.NotSupportedException;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -13,8 +14,16 @@ public class LogConverterConfiguration {
     @Bean
     @ConditionalOnMissingBean(LoggerConverter.class)
     public ConverterService disabledLoggerConverter() {
-        return logMessage -> {
-            throw new NotSupportedException("DisabledConverterServiceImpl");
+        return new ConverterService() {
+            @Override
+            public boolean supports(LogMessage logMessage) {
+                return false;
+            }
+
+            @Override
+            public void convertAndPersist(LogMessage logMessage) throws Exception {
+                throw new NotSupportedException("DisabledConverterServiceImpl");
+            }
         };
     }
 }
