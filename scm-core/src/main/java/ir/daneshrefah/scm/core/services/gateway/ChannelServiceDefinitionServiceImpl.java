824b46885365af94fc75992aafc84bb17907857c
A.Rayani <alireza.rayani@gmail.com>
2025-10-28 10:52:21 +0330

    - Updated `DefinitionRequest` type from `JAVA_MULTIPLE_ROUTE` to `MULTIPLE_ROUTE_CONFIG` in `ChannelServiceDefinitionServiceImpl`.

--- scm-core/src/main/java/ir/daneshrefah/scm/core/services/gateway/ChannelServiceDefinitionServiceImpl.java
+++ scm-core/src/main/java/ir/daneshrefah/scm/core/services/gateway/ChannelServiceDefinitionServiceImpl.java
@@ -119,7 +119,7 @@ public class ChannelServiceDefinitionServiceImpl implements ChannelServiceDefini
             javaMultiRouteDefinitionDetail.setContextPath(request.getContextPath());
             javaMultiRouteDefinitionDetail.setMultiRouteDetails(multiRouteDetails);
             DefinitionRequest definitionRequest = request.getDefinition();
-            definitionRequest.setType(DefinitionType.JAVA_MULTIPLE_ROUTE);
+            definitionRequest.setType(DefinitionType.MULTIPLE_ROUTE_CONFIG);
             definitionRequest.setDetails(objectMapper.valueToTree(javaMultiRouteDefinitionDetail));
             DefinitionResponse definitionResponse = definitionService.createDefinition(definitionRequest);
             definitionEntity = definitionMapper.toEntity(definitionResponse);
