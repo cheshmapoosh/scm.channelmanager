ac325b0e50356781d6bf20b7099b242f7951e4e6
esmaeili.z <esmaeili.z@daneshrefah.ir>
2025-12-29 13:47:14 +0330

    add nab error to  MessageStatus

--- scm-common/src/main/java/ir/daneshrefah/scm/common/model/message/MessageStatus.java
+++ scm-common/src/main/java/ir/daneshrefah/scm/common/model/message/MessageStatus.java
@@ -17,7 +17,8 @@ public enum MessageStatus {
 
     SC_PROCESSING("sc_prg"), SC_SUCCESS("sc_scs"), SC_UNAUTHORIZED("sc_uat"), SC_ACCESS_DENIED("sc_acd"),
     SC_NOT_FOUND("sc_nfd"), SC_ERROR_VALIDATION("sc_evl"), SC_ERROR_SYSTEM("sc_esy"), SC_ERROR_BUSINESS("sc_ebz"),
-    SC_ERROR_UNREACHABLE_PROVIDER("sc_eup"), SC_ERROR_DATA_INTEGRITY_VIOLATION("sc_div");
+    SC_ERROR_UNREACHABLE_PROVIDER("sc_eup"), SC_ERROR_DATA_INTEGRITY_VIOLATION("sc_div"),
+    INVALID_SOURCE_ACCOUNT("nab_acc");
 
     private final String code;
 
