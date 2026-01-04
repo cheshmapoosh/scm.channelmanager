e2f5419bc896f4711950504ea492fd0a64c0bd19
esmaeili.z <esmaeili.z@daneshrefah.ir>
2026-01-04 14:41:18 +0330

    carFinesInq

--- scm-plugin-camel-components/src/main/java/ir/daneshrefah/scm/plugin/camel/component/tcp/nab/atps/AtpsProducer.java
+++ scm-plugin-camel-components/src/main/java/ir/daneshrefah/scm/plugin/camel/component/tcp/nab/atps/AtpsProducer.java
@@ -17,6 +17,7 @@ import java.nio.charset.Charset;
 import java.util.Date;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
