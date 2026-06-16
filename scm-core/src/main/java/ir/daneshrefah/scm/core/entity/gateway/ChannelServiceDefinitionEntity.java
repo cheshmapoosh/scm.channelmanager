aca35d5aa8da8b3f3944cd605ffd17cec034db76
behzaman.m <behzaman.m@daneshrefah.ir>
2026-06-16 08:19:52 +0330

    fix: handle unknown channel service definition types gracefully

--- scm-core/src/main/java/ir/daneshrefah/scm/core/entity/gateway/ChannelServiceDefinitionEntity.java
+++ scm-core/src/main/java/ir/daneshrefah/scm/core/entity/gateway/ChannelServiceDefinitionEntity.java
@@ -34,7 +34,7 @@ public class ChannelServiceDefinitionEntity extends AbstractAuditableEntity<Stri
     @JoinColumn(name = "GATEWAY_CHANNEL_ID", nullable = false, foreignKey = @ForeignKey(name = "FK_CHN_SVC_DEF_ON_GTW_CHN"))
     private GatewayChannelEntity gatewayChannel;
 
-    @Enumerated(EnumType.STRING)
+    @Convert(converter = ChannelServiceDefinitionTypeConverter.class)
     @Size(max = 20)
     @Column(nullable = false, length = 20)
     private ChannelServiceDefinitionType type;
